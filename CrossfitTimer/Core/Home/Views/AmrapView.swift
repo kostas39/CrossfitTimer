b1eaabd883b055606d1a894c92d9cea710b9b0ae
konstantinos koliolios <kolioliosk@gmail.com>
2024-05-11 12:22:28 +0100

    changes in amrapview

--- CrossfitTimer/Core/Home/Views/AmrapView.swift
+++ CrossfitTimer/Core/Home/Views/AmrapView.swift
@@ -11,6 +11,8 @@ struct AmrapView: View {
     @State private var timeRemaining: Int = 600 // Example starting time (10 minutes in seconds)
     @State private var isActive = false // Controls the timer activation
     @State private var progress: CGFloat = 1.0 // For the circular progress view
+    @State private var timerDuration: Double = 10.0 // Default duration in minutes for the slider
+
 
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
@@ -19,6 +21,12 @@ struct AmrapView: View {
             Color.black.edgesIgnoringSafeArea(.all) // Full background color
 
             VStack {
+                Text("AMRAP")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding()
+
+                
                 ZStack {
                     Circle() // Background circle
                         .stroke(lineWidth: 20)
@@ -38,14 +46,34 @@ struct AmrapView: View {
                 }
                 .frame(width: 200, height: 200)
                 .padding()
+                
+                
+                Text("Duration")
+                    .font(.title)
+                    .foregroundStyle(.white)
+                    .padding()
+                
+                Slider(value: $timerDuration, in: 1...60, step: 1) {
+                                    Text("Duration")
+                                } minimumValueLabel: {
+                                    Text("1m")
+                                } maximumValueLabel: {
+                                    Text("60m")
+                                }
+                                .onChange(of: timerDuration) { newValue in
+                                    timeRemaining = Int(newValue) * 60
+                                    progress = 1.0
+                                }
+                                .padding()
 
-                HStack {
+                HStack(spacing: 40) {
                     Button(action: {
                         self.isActive.toggle()
                     }) {
                         Text(isActive ? "PAUSE" : "START")
                             .foregroundColor(.white)
-                            .padding()
+                            .padding(.vertical, 12)
+                            .padding(.horizontal, 30)
                             .background(isActive ? Color.red : Color.green)
                             .clipShape(RoundedRectangle(cornerRadius: 10))
                     }
@@ -54,7 +82,8 @@ struct AmrapView: View {
                         self.resetTimer()
                     }
                     .foregroundColor(.white)
-                    .padding()
+                    .padding(.vertical, 12)
+                    .padding(.horizontal, 30)
                     .background(Color.blue)
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
--- CrossfitTimer/Core/Home/Views/ForTimeView.swift
+++ CrossfitTimer/Core/Home/Views/ForTimeView.swift
@@ -16,3 +16,5 @@ struct ForTimeView: View {
 #Preview {
     ForTimeView()
 }
+
+
